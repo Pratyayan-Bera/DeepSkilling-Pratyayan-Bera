85aa45c63417e994aeb04597e4aea4eb10f87d9b
Pratyayan-Bera <pratyayanbera2003@gmail.com>
2026-06-20 12:07:43 +0530

    week1 tasks completed

--- EngineeringConcepts/DataStructuresAndAlgorithms/EcommerceSearch/obj/Debug/net10.0/EcommerceSearch.AssemblyInfo.cs
+++ EngineeringConcepts/DataStructuresAndAlgorithms/EcommerceSearch/obj/Debug/net10.0/EcommerceSearch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceSearch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f047ae1c5a5d4aff3a12a837b8133ab0856fb7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceSearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceSearch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
